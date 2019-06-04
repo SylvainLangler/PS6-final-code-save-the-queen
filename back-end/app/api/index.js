fca4bdc2c09bab634758a414d122c69c29533cf5
davidbisegna <david.bisegna@etu.univ-cotedazur.fr>
2019-06-04 19:09:22 +0200

    re added temporary connection route

--- back-end/app/api/index.js
+++ back-end/app/api/index.js
@@ -5,6 +5,7 @@ const AgencyRouter = require('./agencies');
 const FormerStudentRouter = require('./former-students');
 const IncrementRouter = require('./increment');
 const AdminRouter = require('./admin');
+const ConnectionRouter = require('./connection');
 
 const router = new Router();
 router.get('/status', (req, res) => res.status(200).json('ok'));
@@ -14,5 +15,6 @@ router.use('/agencies', AgencyRouter);
 router.use('/former-students', FormerStudentRouter);
 router.use('/increment', IncrementRouter);
 router.use('/admin', AdminRouter);
+router.use('/connection', ConnectionRouter);
 
 module.exports = router;
