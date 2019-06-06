b46f491010bf63891e8fc38f66da036fb7f1087b
davidbisegna <david.bisegna@etu.univ-cotedazur.fr>
2019-06-06 12:37:51 +0200

    Fixed bug in routes index.js

--- back-end/app/api/index.js
+++ back-end/app/api/index.js
@@ -5,7 +5,6 @@ const AgencyRouter = require('./agencies');
 const FormerStudentRouter = require('./former-students');
 const IncrementRouter = require('./increment');
 const AdminRouter = require('./admin');
-const ConnectionRouter = require('./connection');
 
 const router = new Router();
 router.get('/status', (req, res) => res.status(200).json('ok'));
@@ -15,6 +14,5 @@ router.use('/agencies', AgencyRouter);
 router.use('/former-students', FormerStudentRouter);
 router.use('/increment', IncrementRouter);
 router.use('/admin', AdminRouter);
-router.use('/connection', ConnectionRouter);
 
 module.exports = router;
